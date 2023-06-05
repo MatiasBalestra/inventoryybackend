5d20da4c8f9db31cec7c42486bdf800773fc0ab5
Matias Balestra <mbalestra@applica.dev>
2023-06-05 05:14:14 -0300

    se añade actualizar categoria

--- src/main/java/com/company/inventory/controler/CategoryRestController.java
+++ src/main/java/com/company/inventory/controler/CategoryRestController.java
@@ -51,4 +51,20 @@ public class CategoryRestController {
 
     }
 
+    /**
+     * update categories
+     * @param category
+     * @param id
+     * @return
+     */
+    @PutMapping("/categories/{id}")
+    public ResponseEntity<CategoryResponseRest> update(@RequestBody Category category, @PathVariable Long id) {
+        ResponseEntity<CategoryResponseRest> response = service.update(category, id);
+        return response;
+
+    }
+
+
+
+
 }
